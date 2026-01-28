84437d1006e524b2c9a87e877cf7d2d07a4bb41a
MrKGhasemi <mr.keshtgar.ghasemi@gmail.com>
2026-01-28 14:44:05 +0330

    Update configs.py

--- config/configs.py
+++ config/configs.py
@@ -3,7 +3,8 @@ BLIP_MODEL_ID = "Salesforce/blip-image-captioning-large"
 SPACY_MODEL = "en_core_web_lg"
 
 GROUNDING_DINO_CONFIG_PATH = "config/GroundingDINO_SwinB_cfg.py"  # or "config/GroundingDINO_SwinT_OGC.py"
-GROUNDING_DINO_CHECKPOINT_PATH = "weights/groundingdino_swinb_cogcoor.pth" # or "weights/groundingdino_swint_ogc.pth"
+# or "weights/groundingdino_swint_ogc.pth"
+GROUNDING_DINO_CHECKPOINT_PATH = "weights/groundingdino_swinb_cogcoor.pth"
 SAM_CHECKPOINT_PATH = "weights/sam_vit_h_4b8939.pth"
 SAM_ENCODER_VERSION = "vit_h"
 
@@ -19,14 +20,13 @@ BLIP_MAX_TOKENS = 50
 PERSON_REPLACE_LIST = ['man', 'woman', 'child', 'boy', 'girl', 'people']
 
 # LLM API Settings (for 'llm' mode)
-LLM_MODEL_NAME = "gemini-2.0-flash"
-LLM_SYSTEM_PROMPT = """You are an image captioner model; at starting point of a semantic segmenation workflow. 
-you should deeply look at the image and find all destinct and unique objects inside;  
-these names will give to Grounding_DINO and SAM to find place of object and then segment them. 
-just bring the saparated-camma list of 'class_names' you found. nothing else.
-if you found human inside, mention as 'person'."""
-LLM_USER_PROMPT = "bring objects list inside the image. "
+LLM_MODEL_NAME = "gemini-3-flash-preview"
+LLM_SYSTEM_PROMPT = """You are an object detection assistant. 
+generate a detailed caption for the provided image, describing all distinct, trackable physical objects in the foreground (e.g., person, car, ball). 
+Ignore background scenery (sky, ground, wall, building), clothing parts (pocket, sleeve), and abstract concepts.
+Return ONLY the caption without any additional text."""
+LLM_USER_PROMPT = "give the caption for this image including all objects"
 
 # Visualization & Output
 SAVE_FIGURES = True
-MASK_ALPHA = 0.5 # Transparency
+MASK_ALPHA = 0.5  # Transparency
